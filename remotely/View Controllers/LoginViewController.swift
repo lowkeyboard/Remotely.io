109ad48f46135df640a789f2eaf80b5b7c37de4d
lowkeyboard <caqlarybak@gmail.com>
2022-03-19 20:18:39 +0300

    onboard_screen: labels added

--- remotely/View Controllers/LoginViewController.swift
+++ remotely/View Controllers/LoginViewController.swift
@@ -27,6 +27,29 @@ class LoginViewController: UIViewController {
         return welcomeLabel
     }()
     
+    private let titleLabel: UILabel = {
+        let titleLabel = UILabel()
+        titleLabel.text = "Remotely.io"
+        titleLabel.textColor = .RT_White
+        titleLabel.font = UIFont(name: "Avenir Next Bold", size: 35)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        return titleLabel
+    }()
+    
+    private let descriptionLabel: UILabel = {
+        let descriptionLabel = UILabel()
+        descriptionLabel.text = "We serve you with the best gadgets for your\n home workspace"
+        descriptionLabel.textColor = .RT_GreyLight
+        descriptionLabel.font = UIFont(name: "Avenir Medium", size: 14)
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        descriptionLabel.numberOfLines = 0
+       
+
+        return descriptionLabel
+    }()
+    
+    
     private let imageView: UIImageView = {
         let imageView = UIImageView(image: UIImage(named: "IM_Onboard"))
         imageView.translatesAutoresizingMaskIntoConstraints = false
@@ -42,6 +65,8 @@ class LoginViewController: UIViewController {
         view.addSubview(imageView)
 
         imageView.addSubview(welcomeLabel)
+        imageView.addSubview(titleLabel)
+        imageView.addSubview(descriptionLabel)
         
         addConstraints()
         
@@ -51,15 +76,33 @@ class LoginViewController: UIViewController {
         var constraints = [NSLayoutConstraint]()
         
      //   add
+        //Imageview
         constraints.append(imageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor))
         constraints.append(imageView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor))
         constraints.append(imageView.bottomAnchor.constraint(equalTo: view.bottomAnchor))
         constraints.append(imageView.topAnchor.constraint(equalTo: view.topAnchor))
         
+        //welcomeLabel
+        
         constraints.append(welcomeLabel.leadingAnchor.constraint(equalTo: imageView.leadingAnchor,constant: 16))
 
         constraints.append(welcomeLabel.centerXAnchor.constraint(equalTo: imageView.centerXAnchor, constant: -50))
-        constraints.append(welcomeLabel.centerYAnchor.constraint(equalTo: imageView.centerYAnchor, constant: 150))
+        constraints.append(welcomeLabel.centerYAnchor.constraint(equalTo: imageView.centerYAnchor, constant: 140))
+        
+        //titleLabel
+        
+        constraints.append(titleLabel.leadingAnchor.constraint(equalTo: welcomeLabel.leadingAnchor))
+
+        constraints.append(titleLabel.centerXAnchor.constraint(equalTo: welcomeLabel.centerXAnchor))
+        constraints.append(titleLabel.centerYAnchor.constraint(equalTo: welcomeLabel.centerYAnchor, constant: 35))
+        
+        //descriptionLabel
+        constraints.append(descriptionLabel.leadingAnchor.constraint(equalTo: welcomeLabel.leadingAnchor))
+
+        constraints.append(descriptionLabel.widthAnchor.constraint(equalTo: imageView.safeAreaLayoutGuide.widthAnchor))
+
+        constraints.append(descriptionLabel.centerYAnchor.constraint(equalTo: titleLabel.centerYAnchor, constant: 35))
+
 
         //Activate (Applying)
         NSLayoutConstraint.activate(constraints)
@@ -109,3 +152,10 @@ class LoginViewController: UIViewController {
 
 }
 
+extension UILabel {
+    func addCharactersSpacing(spacing:CGFloat, text:String) {
+        let attributedString = NSMutableAttributedString(string: text)
+        attributedString.addAttribute(NSAttributedString.Key.kern, value: spacing, range: NSMakeRange(0, text.count-1))
+        self.attributedText = attributedString
+    }
+}
