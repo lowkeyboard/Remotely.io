1f40d8e559a70a0d7550b1019dd1e02f2c703312
lowkeyboard <caqlarybak@gmail.com>
2022-07-31 20:42:36 +0300

    mvvm example deleted

--- remotely/Features/Home/HomeViewController.swift
+++ remotely/Features/Home/HomeViewController.swift
@@ -8,28 +8,14 @@
 import UIKit
 
 class HomeViewController: UIViewController, Coordinating {
-    var coordinator: Coordinator?
-    @IBOutlet private weak var movieTitleLabel: UILabel!
-    @IBOutlet private weak var artistNameLabel: UILabel!
-    @IBOutlet private weak var genreLabel: UILabel!
 
-    var viewModel: MovieDetailViewModelProtocol!
+    var coordinator: Coordinator?
+
+     var viewModel: MovieDetailViewModel?
 
     override func viewDidLoad() {
         title = "Home view controller"
         view.backgroundColor = .RTWhite
-        viewModel.delegate = self
-
     }
-}
 
-extension HomeViewController: MovieDetailViewModelDelegate {
-
-    func showDetail(_ presentation: MovieDetailPresentation) {
-        movieTitleLabel.text = presentation.title
-        artistNameLabel.text = presentation.artistName
-        genreLabel.text = presentation.genre
-
-        print("\(movieTitleLabel.text) \n \(artistNameLabel.text) \n \(genreLabel.text)")
-    }
 }
